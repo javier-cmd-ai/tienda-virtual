7fde9f8d9b409614e33b11ba3b390e6ae08df7a0
javier-cmd-ai <ranfimanuelparraandrade@gmail.com>
2024-12-01 07:24:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click', function(){
+	const mostrar = document.getElementById('mostrar').innerHTML = `
+		Mi correo electronico es elias@gmail.com mi numero de telefono es
+		03874647346
+	`
+	const btn = document.getElementById('btn').innerHTML = 'Less info'
+})
